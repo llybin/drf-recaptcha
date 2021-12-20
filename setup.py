15881eb1052853120eb53b5c9fc509bb19dea856
Benoît Vinot <benoit.vinot@roseautechnologies.com>
2021-12-20 13:30:52 +0100

    Remove Django 3.0

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         "Intended Audience :: Developers",
         "Framework :: Django",
         "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
         "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
