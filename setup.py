8da234a6ce5fef5ac7d9feac5d4d91e980243db6
Lev Lybin <lev.lybin@gmail.com>
2022-10-25 20:22:44 +0700

    Add support drf314 tests (#22)
    * Add support for Django 4.2, DRF 3.14, Python 3.11
    
    * Add support for DRF 3.14

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(f):
 
 setup(
     name="drf-recaptcha",
-    version="2.2.1",
+    version="2.2.2",
     description="Django rest framework recaptcha field serializer.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
