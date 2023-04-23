7ad060dca82fa470512f94ba48cf3b39a9bc657d
Lev Lybin <lev.lybin@gmail.com>
2023-04-23 12:25:39 +0700

    Upgrade version to 2.2.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(f):
 
 setup(
     name="drf-recaptcha",
-    version="2.2.2",
+    version="2.2.3",
     description="Django rest framework recaptcha field serializer.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
