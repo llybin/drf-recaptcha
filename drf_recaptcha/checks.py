b2ad9c6a89978fe93138f161eaeb11f5555393e0
Ben Timby <btimby@gmail.com>
2024-05-10 15:43:28 -0400

    Report error via return value.

--- drf_recaptcha/checks.py
+++ drf_recaptcha/checks.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.checks import Tags, Warning, register
+from django.core.checks import Tags, Error, Warning, register
 from django.core.exceptions import ImproperlyConfigured
 
 from drf_recaptcha.constants import TEST_V2_SECRET_KEY
@@ -15,9 +15,11 @@ def recaptcha_system_check(app_configs, **kwargs):
 
     secret_key = getattr(settings, "DRF_RECAPTCHA_SECRET_KEY", None)
     if not secret_key:
-        raise ImproperlyConfigured("settings.DRF_RECAPTCHA_SECRET_KEY must be set.")
+        errors.append(
+            Error("settings.DRF_RECAPTCHA_SECRET_KEY must be set."),
+        )
 
-    if secret_key == TEST_V2_SECRET_KEY:
+    elif secret_key == TEST_V2_SECRET_KEY:
         errors.append(
             Warning(
                 "Google test key for reCAPTCHA v2 is used now.\n"
