032ebd91d279477e6f4eea1efd6c53001a96e4f1
Lev Lybin <lev.lybin@gmail.com>
2024-09-06 16:23:12 +0700

    Bump 3.2.0, setuptools==71.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(f):
 
 setup(
     name="drf-recaptcha",
-    version="3.1.1",
+    version="3.2.0",
     description="Django rest framework recaptcha field serializer.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
